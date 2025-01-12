eccecbaa96f88f746271a2c26c46d79e756e0381
vijaypanchal05 <vijaypanchal05@gmail.com>
2025-01-12 15:24:47 +0530

    cc

--- routes.js
+++ routes.js
@@ -4,17 +4,11 @@ let userRoutes = require("./routes/users");
 
 let roleRoutes = require("./routes/role");
 let moduleRoutes = require("./routes/moduleAccess");
-let welcomeMessage = require("./routes/welcomeMsg");
 
 
 module.exports = (app) => {
   // admin section routes
   app.use("/api/admin/user", userRoutes);
   app.use("/api/admin/role", roleRoutes);
-  // app.use("/api/zoom", zoomRoutes);
-  
-  // app.use("/api/super/user", superUserRoutes);
-  // app.use("/api/sys/user", sysUserRoutes);
-  // app.use("/api/moduleAccess", moduleRoutes);
-  // app.use("/api/welcomeMessage", welcomeMessage);  
+
 };

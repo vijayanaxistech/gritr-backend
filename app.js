85d4efd3d45a4e51e07ba49db0122b35f1f7bf70
vijaypanchal05 <vijaypanchal05@gmail.com>
2025-02-17 23:35:23 +0530

    cc

--- app.js
+++ app.js
@@ -9,31 +9,10 @@ const compression = require("compression");
 const morgan = require("morgan");
 const swaggerUi = require("swagger-ui-express");
 const dotenv = require("dotenv");
-const os = require("os");
 
-// Force production environment
-process.env.NODE_ENV = "production";
-
-// Load production environment variables
-dotenv.config({ path: `.env.production` });
-
-// Function to get the server's local IP address
-const getLocalIP = () => {
-  const networkInterfaces = os.networkInterfaces();
-  for (const interfaceKey in networkInterfaces) {
-    const networkInterface = networkInterfaces[interfaceKey];
-    for (const iface of networkInterface) {
-      // Skip internal interfaces (e.g., "lo" on Unix systems)
-      if (!iface.internal && iface.family === "IPv4") {
-        return iface.address;
-      }
-    }
-  }
-  return null; // Return null if no external IP is found
-};
-
-const serverIp = getLocalIP();
-console.log(`Server's local IP address: ${serverIp}`);
+// Load environment variables dynamically
+const environment = process.env.NODE_ENV || "development";
+dotenv.config({ path: `.env.${environment}` });
 
 // Import custom files
 const errorMiddleware = require("./middleware/error");
@@ -51,16 +30,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(compression());
 app.use(fileupload());
+app.use(morgan("dev"));
 
-// Disable logging (morgan) in production for performance
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
-
-// Serve API documentation (disabled in production)
-if (process.env.NODE_ENV === "development") {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-}
+// Serve API documentation
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Serve static files from public directory
 app.use(express.static(path.join(__dirname, "public")));
@@ -74,8 +47,6 @@ app.use(errorMiddleware);
 // Connect to the database and start the server
 connectDB()
   .then(() => {
-    console.log(`Server's local IP address: ${serverIp}`);
-
     console.log("Database connected successfully");
     app.listen(PORT, () => {
       console.log(`Server started successfully on ${process.env.BASE_URL}`);
